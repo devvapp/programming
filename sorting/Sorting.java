9d864aa16d6e9fecc099e8adcaf45581f96be496
Aneesh Kolloju <devvapp@gmail.com>
2017-11-26 17:25:59 -0600

    Quick sort

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -65,18 +65,91 @@ public class Sorting {
         }
     }
 
+    /**
+     * Quick sort - Quick sort follows merge sorts divide and conquer method.
+     * 
+     * 
+     * Time complexity - 
+     * Space complexity - 
+     */
     public void quickSort(int[] arr){
-        
+        quickSort(arr, 0, arr.length-1);
     }
 
+    /**
+     * Recursively method for quick sort
+     */
+    public void quickSort(int[] arr, int low, int high){
+        if(low<high){
+            int pivot = partition(arr, low, high);
+
+            quickSort(arr, low, pivot-1);
+            quickSort(arr, pivot+1, high);
+        }
+    }
+
+    /**
+     * 
+     */
+    public int partition(int[] arr, int low, int high){
+
+        /*
+            Pivot can be chose in following ways - 
+            1. Pick first element as pivot.
+            2. Pick last element as pivot
+            3. Pick a random element as pivot.
+            4. Pick median as pivot.
+
+            Lets choose second option
+        */
+        int pivot = arr[high];
+        
+        int i = low - 1;
+
+        for(int j=low; j<high ; j++){
+            if(arr[j] <= pivot){
+                i++;
+
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        
+        // swap arr[i+1] and arr[high]
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
+
+    /**
+     * Merge sort - 
+     * 
+     * Time complexity - 
+     * Space complexity - 
+     */
     public void mergeSort(int[] arr){
         
     }
 
+    /**
+     * Bubble sort - 
+     * 
+     * Time complexity - 
+     * Space complexity - 
+     */
     public void bubbleSort(int[] arr){
         
     }
 
+    /**
+     * Heap sort - 
+     * 
+     * Time complexity - 
+     * Space complexity - 
+     */
     public void heapSort(int[] arr){
         
     }
